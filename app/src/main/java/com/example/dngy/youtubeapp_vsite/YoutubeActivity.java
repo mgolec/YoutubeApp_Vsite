ac4c8147bcd8b68ce952a3edeb01a1a7f396c706
mgolec <marko.golec@gmail.com>
2016-04-11 16:53:09 +0200

    YoutubeActivity;                    Implementiranje metoda iz API-ja;                    Inicijaliziranje PlayerView-a;                    Kreirane poruke o inicijalizaciji - toast message;                    ako je season restored - nastavak izvođenja videa;                    playbackEventListeners i stateChangeListeners override metode napisane - nema više greške kod deklariranja klase;                    Test metoda iz listenera sa toast porukama;

--- app/src/main/java/com/example/dngy/youtubeapp_vsite/YoutubeActivity.java
+++ app/src/main/java/com/example/dngy/youtubeapp_vsite/YoutubeActivity.java
@@ -2,9 +2,12 @@ package com.example.dngy.youtubeapp_vsite;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity
             implements YouTubePlayer.OnInitializedListener
@@ -18,6 +21,10 @@ public class YoutubeActivity extends YouTubeBaseActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube);
+
+        YouTubePlayerView youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player);
+        youTubePlayerView.initialize(GOOGLE_API_KEY, this);
+
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
@@ -31,4 +38,83 @@ public class YoutubeActivity extends YouTubeBaseActivity
 //        });
     }
 
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+        Toast.makeText(this, "Uspješna inicijalizacija Youtube Playera!", Toast.LENGTH_LONG).show();
+        youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
+
+        if (!wasRestored){
+            youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID);
+        }
+    }
+
+    // YouTubePlayer.PlaybackEventListener
+    // https://developers.google.com/youtube/android/player/reference/com/google/android/youtube/player/YouTubePlayer.PlaybackEventListener
+    private YouTubePlayer.PlaybackEventListener playbackEventListener = new YouTubePlayer.PlaybackEventListener() {
+        @Override
+        public void onPlaying() {
+            Toast.makeText(YoutubeActivity.this, "Video je pokrenut", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onPaused() {
+            Toast.makeText(YoutubeActivity.this, "Video je pauziran", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onStopped() {
+
+        }
+
+        @Override
+        public void onBuffering(boolean b) {
+
+        }
+
+        @Override
+        public void onSeekTo(int i) {
+
+        }
+    };
+
+    // YouTubePlayer.PlayerStateChangeListener
+    // https://developers.google.com/youtube/android/player/reference/com/google/android/youtube/player/YouTubePlayer.PlayerStateChangeListener
+    YouTubePlayer.PlayerStateChangeListener playerStateChangeListener = new YouTubePlayer.PlayerStateChangeListener() {
+        @Override
+        public void onLoading() {
+
+        }
+
+        @Override
+        public void onLoaded(String s) {
+
+        }
+
+        @Override
+        public void onAdStarted() {
+            Toast.makeText(YoutubeActivity.this, "Klikni na oglas!", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onVideoStarted() {
+            Toast.makeText(YoutubeActivity.this, "Pokretanje videa", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onVideoEnded() {
+
+        }
+
+        @Override
+        public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+        }
+    };
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Toast.makeText(this, "Neuspješna inicijalizacija Youtube Playera!", Toast.LENGTH_LONG).show();
+
+    }
 }
